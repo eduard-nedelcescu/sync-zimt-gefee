0520f6b353224d80ab9cecb43e53d89c141d2cbc
Eduard Nedelcescu <eddie@swazm.com>
2020-05-06 19:32:36 +0300

    implement get_streets/:id route

--- src/routes/gefee.js
+++ src/routes/gefee.js
@@ -24,6 +24,22 @@ module.exports = server => {
     }
   });
 
+  server.get('/api/gefee/get_streets/:locality_id', requiresSession, async (req, res) => {
+    try {
+      const result = await gefee.get(process.env.ENDPOINT_GET_STREETS, {
+        app_key: process.env.GEFFEE_APP_KEY,
+        session_token: gefee.AuthenticationToken,
+        locality_id: req.params.locality_id
+      });
+
+      res.json(result.data);
+
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('Could not find streets...')
+    }
+  });
+
   server.post('/api/gefee/add_partner_business', requiresSession, async (req, res) => {
     console.log(req.body);
     const base64BodyData = Buffer.from(JSON.stringify({data: req.body})).toString('base64');
